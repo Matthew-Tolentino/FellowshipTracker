6e4c574a4596743d869676aaee41d7ba8baabaf2
Matthew <lemtolen@ucsc.edu>
2024-10-11 12:22:47 -0700

    Center map and get default icon imported

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import MapContainer from './components/MapContainer/MapContainer';
 import Map from './components/Map/Map';
 import './App.scss';
+import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
       <div className="App">
         <h1>Fellowship Progress</h1>
         {/* <MapContainer /> */}
         <Map />
       </div>
-    </div>
+    // </div>
   );
 }
 
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -5,7 +5,21 @@ import 'leaflet/dist/leaflet.css';
 import './Map.scss';
 import LOTRmap from '../../imgs/MiddleEarth.jpg'
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
 const imageBounds: [[number, number], [number, number]] = [[0,0], [2400, 2424]];
+
+const iconMarker = new L.Icon({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41], // Default size of the Leaflet marker
+  iconAnchor: [12, 41], // Anchor point of the marker icon
+  popupAnchor: [1, -34], // Popup position relative to the icon
+  shadowSize: [41, 41], // Shadow size of the marker icon
+});
 
 const Map: React.FC = () => {
   const center: LatLngExpression = [1200, 1212];
@@ -34,12 +48,12 @@ const Map: React.FC = () => {
         minZoom={-1}
         maxZoom={10}
         crs={L.CRS.Simple}
-        style={{height: '50vh', width: '75vw'}}
+        style={{height: '100%', width: '75%'}}
         maxBoundsViscosity={1}
       >
         <ImageOverlay url={LOTRmap} bounds={imageBounds}/>
         <FitBounds />
-        <Marker position={center}>
+        <Marker position={center} icon={iconMarker}>
           <Popup>This is a marker on the image for testing</Popup>
         </Marker>
         <Polyline positions={pathCoordinates} color='red'>
