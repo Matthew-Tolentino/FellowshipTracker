71e1ad8aa5e80c639a8fa443c8690ac132254e94
matthew-tolentino-333 <lemtolen@ucsc.edu>
2024-10-16 21:52:22 -0700

    code to set vh to screen space

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,21 @@ function App() {
     }
   }, [fellowship]);
 
+  useEffect(() => {
+    const setViewportHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    setViewportHeight();
+
+    window.addEventListener('resize', setViewportHeight);
+
+    return () => {
+      window.removeEventListener('resize', setViewportHeight);
+    };
+  }, []);
+
   return (
     <>
       {/* <div className="App p-background">         */}
