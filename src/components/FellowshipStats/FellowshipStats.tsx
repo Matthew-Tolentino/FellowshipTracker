668efcb7b22a87f0a2e68fc1177bc646c7bd86d7
matthew-tolentino-333 <lemtolen@ucsc.edu>
2024-10-20 22:02:48 -0700

    truncate stats

--- src/components/FellowshipStats/FellowshipStats.tsx
+++ src/components/FellowshipStats/FellowshipStats.tsx
@@ -13,10 +13,10 @@ const FellowshipStats: React.FC<IFellowshipStats> = (({fellowship}) => {
       <h1>Fellowship Progress</h1>
       <div className='stats'>
         <span>
-          Day {Math.floor(dayDiff(fellowship.startDate, new Date()))} of {dayDiff(fellowship.startDate, fellowship.endDate).toFixed(2)}
+          Day {Math.floor(dayDiff(fellowship.startDate, new Date()))} of {dayDiff(fellowship.startDate, fellowship.endDate).toFixed(0)}
         </span>
         <GiRing />
-        <span>{fellowship.getTotalMiles()} mi / {fellowship.goal} mi Traveled</span>
+        <span>{fellowship.getTotalMiles().toFixed(2)} mi / {fellowship.goal} mi Traveled</span>
       </div>
     </div>
   )
